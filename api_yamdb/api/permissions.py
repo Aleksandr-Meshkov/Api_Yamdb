a1e6d8bd829c670ec860ca3dc9dc046fe552cb47
Александр Мешков <meshkov.soner1989@yandex.ru>
2022-07-16 17:40:37 +0300

    Добавил пермишин на Админа Модератора и автора

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -11,3 +11,14 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated and request.user.is_admin
         )
+
+
+class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.method == 'POST' and request.user.is_authenticated
+            or obj.author == request.user
+            or request.user.is_admin
+            or request.user.is_moderator
+        )
