b9f1ccb7b05aa5f57054df13584ca0d49cb0a63b
Александр Мешков <meshkov.soner1989@yandex.ru>
2022-07-19 16:50:53 +0300

    JWT-settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig',
     'django_filters',
